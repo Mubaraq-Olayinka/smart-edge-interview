3d1cbafb18fa0d1707e59ad3d6b2ce1ce67d11fd
dexie14 <adeluadeoniye14@gmail.com>
2025-05-27 13:11:36 +0100

    feat: update

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -1,17 +1,39 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 interface Todos {
-    id: string
+  id: string;
+  task: string;
 }
 
 export const apiSlice = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }), 
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
   endpoints: (builder) => ({
     getTodos: builder.query<Todos[], void>({
-        query: () => '/todos',
+      query: () => "/todos",
+    }),
+    createTodo: builder.mutation<Todos, Partial<Todos>>({
+      query: (data) => ({
+        url: `todos`,
+        method: "POST",
+        body: data,
       }),
+    }),
+    editTodo: builder.mutation<Todos, Partial<Todos>>({
+      query: ({ id, ...data }) => ({
+        url: `todos/${id}`,
+        method: "put",
+        body: data,
+      }),
+    }),
+    deleteTodo: builder.mutation<Todos, Partial<Todos>>({
+      query: ({ id }) => ({
+        url: `todos/${id}`,
+        method: "delete",
+      }),
+    }),
   }), // define endpoints later
-})
+});
 
-export const { useGetTodosQuery } = apiSlice
+export const { useGetTodosQuery, useCreateTodoMutation, useEditTodoMutation, useDeleteTodoMutation } =
+  apiSlice;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,97 @@
-'use client'
-import { useGetTodosQuery } from "@/api/apiSlice";
+"use client";
+import {
+  useCreateTodoMutation,
+  useEditTodoMutation,
+  useGetTodosQuery,
+} from "@/api/apiSlice";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface Todos {
+  id: string;
+  task: string;
+}
 
 export default function Home() {
-  const { data, error, isLoading } = useGetTodosQuery();
+  const { data, error, isLoading, refetch } = useGetTodosQuery();
 
-  console.log(data)
+  console.log(data);
+
+  const [mutate] = useCreateTodoMutation();
+  const [editMutate] = useEditTodoMutation();
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    if (data) {
+      setTask(editTask?.task || "");
+    }
+  });
+
+  const [editTask, setEditTask] = useState<Todos | null>(null);
+
+  const handleEditTask = (item: Todos) => {
+    setEditTask(item);
+  };
+
+  const handleAddTask = () => {
+    if (editTask) {
+      editMutate({ id: editTask.id, task });
+    } else {
+      mutate({ task });
+      setTask("");
+    }
+    refetch();
+    setTask("");
+  };
 
   return (
     <div>
+      <h3 className="text-center mt-7">Todo List</h3>
+      <main className="w-[90%] mx-auto mt-5">
+        {isLoading ? (
+          <>loading...</>
+        ) : (
+          data?.map((item: Todos, index) => {
+            return (
+              <div key={index}>
+                <p>{item?.id}</p>
+                <div className="flex items-center gap-2">
+                  <p>{item?.task}</p>
+                  <button
+                    className="bg-yellow-200 p-4 rounded-[10px]"
+                    onClick={() => handleEditTask(item)}
+                  >
+                    Edit task
+                  </button>
+                  <button className="bg-red-200 p-4 rounded-[10px]">
+                    Delete task
+                  </button>
+                </div>
+              </div>
+            );
+          })
+        )}
 
+        <section className="my-20">
+          <div className="flex flex-col gap-3">
+            <label>Task</label>
+            <input
+              type="text"
+              name="task"
+              defaultValue={task}
+              className="border border-gray-300 px-3 py-2 rounded-[10px] focus:outline-none focus:border-blue-500"
+              onChange={(e) => setTask(e.target.value)}
+            />
+          </div>
+
+          <button
+            onClick={handleAddTask}
+            className="bg-green-200 px-4 rounded-[10px] my-5"
+          >
+            Add Task
+          </button>
+        </section>
+      </main>
     </div>
   );
 }
